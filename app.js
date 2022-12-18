9247c0b830d8e87bfe23959b7137934d40a393ed
Daniel RG <21danielrondong@gmail.com>
2022-12-18 23:19:25 +0100

    lab done up to iteration 5

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -14,12 +15,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+//BEER CARD PARTIAL
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
+//HOME ROUTE
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//BEERS ROUTE
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi);
+    res.render('beers', { beer: beersFromApi });
+  })
+  .catch(error => console.log(error));
+})
+
+//RANDOM BEER ROUTE
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log('Random-beer from the database: ', responseFromAPI);
+    res.render('random-beer', responseFromAPI[0]);
+  })
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
